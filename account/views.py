4dc2290d9b7751c3fc37b65f6f0c7b945e199952
Mason Smith <masonsmith40@hotmail.co.uk>
2018-07-19 12:11:29 +0100

    logout and sign in now working properly

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 
 def signup_view(request):
     if request.method == 'POST':
@@ -16,10 +16,11 @@ def signup_view(request):
 		
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid:
-            user  = form.get_user()
-            login(request,user)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
             return redirect('data_display:index')
     else:
         form = AuthenticationForm()
@@ -27,10 +28,7 @@ def login_view(request):
     return render(request,'account/login.html',{'form':form})
 		
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('data_display:index')
-		
+    logout(request)	
     return redirect('data_display:index')
 		
 def index(request):
